e15bc6f0defe7d7844b2e207059989aa74e9159f
Abdul Rehman <pakkhan9009@gmail.com>
2024-04-18 19:31:11 +0500

    js comparisons

---
+++ 01_js_basics/04_comparison.js
@@ -0,0 +1,8 @@
+// T8: Comparisons basics
+console.log(2 > 1);   // greater than
+console.log(1 < 2);   // less than
+console.log(2 >= 1);  // greater than or equal to
+console.log(2 <= 2);  // less than or equal to
+console.log(2 == 4);  // equal to
+console.log(2 != 2);  // not equal to
+
