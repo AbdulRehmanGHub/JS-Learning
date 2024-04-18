20e459d93e52d205c5c3f6f4285b0c2030449de9
Abdul Rehman <pakkhan9009@gmail.com>
2024-04-18 20:45:27 +0500

    comparisons in js

--- 01_js_basics/04_comparison.js
+++ 01_js_basics/04_comparison.js
@@ -1,8 +1,25 @@
 // T8: Comparisons basics
-console.log(2 > 1);   // greater than
-console.log(1 < 2);   // less than
-console.log(2 >= 1);  // greater than or equal to
-console.log(2 <= 2);  // less than or equal to
-console.log(2 == 4);  // equal to
-console.log(2 != 2);  // not equal to
+// console.log(2 > 1);   // greater than
+// console.log(1 < 2);   // less than
+// console.log(2 >= 1);  // greater than or equal to
+// console.log(2 <= 2);  // less than or equal to
+// console.log(2 == 4);  // equal to
+// console.log(2 != 2);  // not equal to
 
+// P2:
+// console.log("02" > 1);
+// console.log(12 < "02");
+
+// P3: Avoid these types of comparisons, because it's becoming more complex. Not any logic of these.
+// console.log(null == null);
+// console.log(null > 0);
+// console.log(null < 2);
+// console.log("-1" < null);
+
+// console.log(undefined == undefined);
+// console.log(undefined > 2);
+// console.log(undefined < 200);
+// console.log("2" < undefined);
+
+// P4: Strictly check comparison ===
+console.log(2 === 2);
