93a35c46884509d9cec469c8170387d90bf9e937
Abdul Rehman <pakkhan9009@gmail.com>
2024-04-21 22:47:41 +0500

    some more dates methods

--- 01_js_basics/09_dates.js
+++ 01_js_basics/09_dates.js
@@ -18,7 +18,17 @@ let myDate = new Date();
 // const myBirthDay = new Date(2000, 0, 30, 12, 20, 0);
 // console.log(myBirthDay.toLocaleString());
 
-let myClassTime = new Date("20-04-2024")
-console.log(myClassTime.toLocaleString());
+// let myClassTime = new Date("04-24-2024")
+// console.log(myClassTime.toLocaleString());
+
+// let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);                               // output like this:  1713721208969
+// console.log(myClassTime.getTime());                     // output like this:  1713898800000
+// console.log(Math.floor(Date.now() / 1000));             // output like this:  1713721150
 
 
+let todayDate = new Date();
+console.log(todayDate.getMonth());                          // output:  3 -- 0 based index
+console.log(todayDate.getFullYear());                       // output:  2024
+console.log(todayDate.getDate());                           // output:  21
