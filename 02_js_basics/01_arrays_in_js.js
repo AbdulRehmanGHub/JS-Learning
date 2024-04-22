7511b190a1d77635205555f27726a5b0e5678b6f
Abdul Rehman <pakkhan9009@gmail.com>
2024-04-22 14:13:20 +0500

    array is js

---
+++ 02_js_basics/01_arrays_in_js.js
@@ -0,0 +1,43 @@
+// T15: basics of Array
+// let newArray = [1,2,3,4,5];
+// console.log(newArray);
+// console.log(newArray[2]);                   // output = 3 -- 0 based index
+
+// const myFavSubjects = ["Computer", "Science", "History"];
+// console.log(myFavSubjects[0]);
+
+// Array Methods
+// let myArray = [10,20,30,40,50];
+// console.log(myArray);
+
+// myArray.push(70);                               // push is used to insert value in array
+// console.log(myArray);
+
+// myArray.pop();                                  // pop is used to remove the last value from the array
+// console.log(myArray);
+
+// myArray.unshift(0);                             // add one more element in the start of array. -- the other elements positions affects, like move to next postitions.
+// console.log(myArray);
+
+// myArray.shift();                                // remove the very first element of array -- 
+// console.log(myArray);
+
+
+// console.log(myArray.includes(4));               // output: false  -- not in array
+// console.log(myArray.includes(20));              // output: true -- in array
+
+// console.log(myArray.indexOf(9));                // output: -1  -- if not in array
+// console.log(myArray.indexOf(30));               // output: 2   -- because 30 is at index 2.
+
+
+// const newArray2 = myArray.join();               
+// console.log(myArray);                           // output: [ 10, 20, 30, 40, 50 ]  -- in array
+// console.log(newArray2);                         // output: 10,20,30,40,50  -- in strings, because join converted this into strings.
+// console.log(typeof newArray2);                  // output: string
+
+
+// slice and splice
+
+
+
+
