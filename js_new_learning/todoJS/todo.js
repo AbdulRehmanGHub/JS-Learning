ce57a286f66dcfe4cce5ee6ff328cdb90b8da11a
abdulrehmanghub <pakkhan9009@gmail.com>
2024-08-24 18:37:14 +0500

    practicing basics of js

---
+++ js_new_learning/todoJS/todo.js
@@ -0,0 +1,37 @@
+let todo = [];
+
+// main menu -- options
+let req = prompt("TODO APP \n Select Options: add, remove, list, quit");
+
+while (true) {
+  // quit option
+  if (req == "quit") {
+    console.log("You've quit the app!");
+    break;
+  }
+
+  // add option
+  if (req == "add") {
+    let addTask = prompt("Enter to Add a Task.");
+    todo.push(addTask);
+    console.log("task added");
+  }
+  
+  // remove option
+  if (req == "remove") {
+    let remTask = prompt("Enter a Task to Remove.");
+    todo.splice(remTask, 1);
+    console.log("task removed");
+  }
+  
+  // list
+  if (req == "list") {
+    console.log(" <---- TODO ITEMS ---- > ");
+    for (i = 0; i < todo.length; i++) {
+      console.log(i, todo[i]);
+    }
+    console.log("----------------");
+  }
+
+  req = prompt("TODO APP \n Select Options: add, remove, list, quit");
+}
