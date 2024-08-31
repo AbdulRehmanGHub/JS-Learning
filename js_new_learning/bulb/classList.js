e49dd8cbfd4e9134bfdf377bba752b9294f085ab
abdulrehmanghub <pakkhan9009@gmail.com>
2024-08-31 11:14:02 +0500

    toggle button

--- js_new_learning/bulb/classList.js
+++ js_new_learning/bulb/classList.js
@@ -1,9 +1,10 @@
-// let btnOn = document.querySelector('.bulb-on');
-// let btnOff = document.querySelector('.bulb-off');
+let btnOn = document.querySelector('.bulb-on');
+let btnOff = document.querySelector('.bulb-off');
 let btn = document.querySelector('#bulb_btn');
 
-btn.addEventListener('click', ()=>{
-    document.querySelector('.bulb-on').style.display = 'block';
-    document.querySelector('.bulb-off').style.display = 'none';
-
+btn.addEventListener('click', () => {
+    btnOn.classList.toggle('bulb-on');
+    btnOn.classList.toggle('bulb-off');
+    btnOff.classList.toggle('bulb-on');
+    btnOff.classList.toggle('bulb-off');
 });
