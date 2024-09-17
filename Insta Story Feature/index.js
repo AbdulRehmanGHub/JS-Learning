cec27f4b730a0b9c29b2a177e280d245dfd5dad0
abdulrehmanghub <pakkhan9009@gmail.com>
2024-09-17 10:35:36 +0500

    Insta Story Mode

---
+++ Insta Story Feature/index.js
@@ -0,0 +1,54 @@
+let storyData = [
+  {
+    dp: "https://images.unsplash.com/photo-1519456264917-42d0aa2e0625?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1661765722951-7c001baba43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1687382111414-7b87afa5da34?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1623170909888-4e97ff277186?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1463674349210-38e4fa154dda?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://plus.unsplash.com/premium_photo-1670282393309-70fd7f8eb1ef?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://images.unsplash.com/photo-1461280360983-bd93eaa5051b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://plus.unsplash.com/premium_photo-1697612942566-e0c243ab512c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    story:
+      "https://plus.unsplash.com/premium_photo-1697612943572-460a55c10ec1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+let stories_Container = document.querySelector(".stories-container");
+let clutter = "";
+
+storyData.forEach(function (elem, idx) {
+  //   console.log(elem, idx);
+  clutter += `<div class="story-box">
+        <img id="${idx}" src="${elem.dp}" alt="img">
+    </div>`;
+});
+
+stories_Container.innerHTML = clutter;
+
+stories_Container.addEventListener("click", function (details) {
+  // console.log(details.target.id);
+  // console.log(storyData[details.target.id]);
+  let value = storyData[details.target.id].story;
+  // console.log(viewStory);
+
+  let viewStory = document.querySelector(".viewstory");
+  viewStory.style.display = "block";
+
+  viewStory.style.backgroundImage = `url(${value})`;
+  setTimeout(function () {
+    viewStory.style.display = "none";
+  }, 5000);
+});
