85050b5eb9106e2255b57985a24f158f837ee590
abdulrehmanghub <pakkhan9009@gmail.com>
2024-09-25 11:35:03 +0500

    Fetch, Async, Promise in JS

--- js_new_learning/api/index.js
+++ js_new_learning/api/index.js
@@ -1,63 +1,219 @@
-// // FETCH API through FETCH, THEN METHOD
-let imgg = document.querySelector(".pic img");
-let divv = document.querySelector(".details");
+{
+  // // Promise
+  // let val = new Promise((res, rej) => {
+  //   if (fasle) {
+  //     return res();
+  //   } else {
+  //     return rej();
+  //   }
+  // });
 
-// function userData(){
-//     fetch('https://randomuser.me/api/')
-//     .then(function(raw){
-//         return raw.json();
-//     })
-//     .then(function(data){
-//         let fullDetails = data.results[0].location.street.name;
-//         let pic = data.results[0].picture.large;
+  // val
+  //   .then(function () {
+  //     console.log("ok");
+  //   })
+  //   .catch(function () {
+  //     console.log("error");
+  //   });
 
-//         console.log(fullDetails);
+  // // Promise Qs 1 - user ask for a number b/w 0-10. Upto 5 reject and more will resolve
 
-//         divv.innerHTML = fullDetails;
+  // let user_que = new Promise((resolve, reject)=>{
+  //   let num = Math.floor(Math.random() * 11);
+  //   console.log(num);
 
-//         imgg.setAttribute('src', `${pic}`);
-//     });
-// };
+  //   if(num > 5){
+  //     return resolve();
+  //   }else if(num <= 5){
+  //     return reject();
+  //   }
+  // });
 
-// userData();
+  // user_que
+  // .then(function(){
+  //   console.log("Query Resolved");
+  // })
 
-// // Number 2 - Fetch, then
-// function uData(){
-//     fetch('https://dummyjson.com/quotes')
-//     .then(function(raw){
-//         return raw.json();
-//     })
-//     .then(function(data){
-//         console.log(data);
+  // .catch(function(){
+  //   console.log("Query Rejected");
+  // });
 
-//         let newQuote = data.quotes;
-//         console.log(newQuote);
 
-//         newQuote.forEach((elem)=>{
-//             let q = elem.quote;
-//             divv.innerHTML += `<p>${q}</p>`;
-//         })
 
-//     });
-// };
 
-// uData();
 
-// // FETCH API through ASYNC, AWAIT METHOD
-async function dailyQuote() {
-  let raw = await fetch("https://dummyjson.com/quotes");
-  let ans = await raw.json();
-  // console.log(ans);
 
-  let quotes = ans.quotes;
 
-  quotes.forEach(function (elem) {
-    // console.log(elem.quote);
-    let q = elem.quote;
-    let id = elem.id;
 
-    divv.innerHTML += `<p>${id} <br> ${q}</p>`;
+
+
+
+  // // Promise Qs 2 - Daily routine tasks
+  // // Get up in monring
+  // // Ready for school
+  // // Do breakfast
+  // // Enter in the school
+  // // Way back to home
+  // // Have a launch
+  // // When night, sleep
+
+  let dailyTasks = new Promise(function (res, rej) {
+    return res("Get up in morning");
   });
-}
 
-dailyQuote();
+  let t2 = dailyTasks.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("Ready for school");
+    });
+  });
+
+  let t3 = t2.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("Do breakfast");
+    });
+  });
+
+  let t4 = t3.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("Enter in the school");
+    });
+  });
+
+  let t5 = t4.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("Way back to home");
+    });
+  });
+
+  let t6 = t5.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("Have a launch");
+    });
+  });
+
+  let t7 = t6.then(function (data) {
+    console.log(data);
+
+    return new Promise(function (res, rej) {
+      return res("When night, sleep");
+    });
+  });
+
+ let error = t7.then(function (data) {
+    console.log(data);
+
+    return new Promise(function(res, rej){
+      return rej("Rejected");
+    })
+  });
+
+  error.catch(function(data){
+    console.log(data);
+  });
+
+
+
+
+
+
+
+
+  // // FETCH API through FETCH, THEN METHOD
+  // let imgg = document.querySelector(".pic img");
+  // let divv = document.querySelector(".details");
+
+  // function userData(){
+  //     fetch('https://randomuser.me/api/')
+  //     .then(function(raw){
+  //         return raw.json();
+  //     })
+  //     .then(function(data){
+  //         let fullDetails = data.results[0].location.street.name;
+  //         let pic = data.results[0].picture.large;
+
+  //         console.log(fullDetails);
+
+  //         divv.innerHTML = fullDetails;
+
+  //         imgg.setAttribute('src', `${pic}`);
+  //     });
+  // };
+
+  // userData();
+
+  // // Number 2 - Fetch, then
+  // function uData(){
+  //     fetch('https://dummyjson.com/quotes')
+  //     .then(function(raw){
+  //         return raw.json();
+  //     })
+  //     .then(function(data){
+  //         console.log(data);
+
+  //         let newQuote = data.quotes;
+  //         console.log(newQuote);
+
+  //         newQuote.forEach((elem)=>{
+  //             let q = elem.quote;
+  //             divv.innerHTML += `<p>${q}</p>`;
+  //         })
+
+  //     });
+  // };
+
+  // uData();
+
+  // // FETCH API through ASYNC, AWAIT METHOD
+  // async function dailyQuote() {
+  //   let raw = await fetch("https://dummyjson.com/quotes");
+  //   let ans = await raw.json();
+  //   // console.log(ans);
+
+  //   let quotes = ans.quotes;
+
+  //   quotes.forEach(function (elem) {
+  //     // console.log(elem.quote);
+  //     let q = elem.quote;
+  //     let id = elem.id;
+
+  //     divv.innerHTML += `<p>${id} <br> ${q}</p>`;
+  //   });
+  // }
+
+  // dailyQuote();
+
+  // // Summary
+  // // 1 - FETCH, THEN
+
+  // function abc(){
+  //     fetch('api link')
+  //     .then(function(raw){
+  //         return raw.json()
+  //     })
+  //     .then(function(data){
+  //         console.log(data);
+  //     });
+  // };
+
+  // abc();
+
+  // // 2 - FETCH, THEN
+
+  // async function abc(){
+  //     let raw = await fetch('api link')
+  //     let ans = await raw.json();
+  //     console.log(ans);
+  // }
+
+  // abc();
+}
