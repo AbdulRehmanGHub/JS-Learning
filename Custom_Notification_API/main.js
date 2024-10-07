45c2ffef053b9669396e2286c40cd31e51f1e354
abdulrehmanghub <pakkhan9009@gmail.com>
2024-10-07 22:35:30 +0500

    Notification Custom API

---
+++ Custom_Notification_API/main.js
@@ -0,0 +1,22 @@
+let inputField = document.querySelector("input");
+
+function notify() {
+  // console.log(inputField);
+  Notification.requestPermission().then(function (results) {
+    if (results === "granted") {
+      popupNotif();
+    } else {
+      console.log("Permission denied");
+    }
+  });
+}
+
+function popupNotif() {
+  const notification = new Notification("New Notification", {
+    body: inputField.value || "This is a default message.",
+  });
+
+  notification.onclick = function () {
+    console.log("Notificaiton clicked");
+  };
+}
